4f842329f8a2d0f5afa7058e988439b40c35b1a4
milad khajavi <mkhajavi5@gmail.com>
2020-08-31 17:01:25 +0430

    insert side drawer navigation ,add portal for drawers

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MainHeader from './MainHeader'
 import {Link} from 'react-router-dom'
 import './MainNavigation.css'
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIelement/Backdrop';
 
 const MainNavigation = (props)=>{
+    const [drawerIsOpen,setDrawer] = useState(false)
+    const openDrawer = ()=>{
+        setDrawer(true)
+    }
+    const closeDrawer = ()=>{
+        setDrawer(false)
+    }
+
     return(
+        <React.Fragment>
+         {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+         {drawerIsOpen && (<SideDrawer>
+             <nav className="main-navigation__drawer-nav">
+                 <NavLinks/>
+             </nav>
+         </SideDrawer>
+         )}       
+
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick={openDrawer}>
                 <span/>
                 <span/>
                 <span/>
@@ -14,10 +34,11 @@ const MainNavigation = (props)=>{
             <h1  className="main-navigation__title">
                 <Link to="/"> your Place</Link>
             </h1>
-            <nav>
-                ...
+            <nav className="main-navigation__header-nav">
+               <NavLinks/>
             </nav>
         </MainHeader>
+        </React.Fragment>
     )
 }
 
---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+import './NavLinks.css'
+const NavLinks = (props)=>{
+    return(
+        <ul className="nav-links">
+            <li>
+                <NavLink to="/" exact>All Users</NavLink>
+            </li>
+            <li>
+                <NavLink to="/uid1/places">My places</NavLink>
+            </li>
+            <li>
+                <NavLink to="/places/new">Add Places</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">Auth</NavLink>
+            </li>
+        </ul>
+    )
+}
+
+export default NavLinks;
