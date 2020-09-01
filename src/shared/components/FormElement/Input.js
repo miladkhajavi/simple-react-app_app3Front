9c55be36c491b32e600e99f126143f2c68bef704
milad khajavi <mkhajavi5@gmail.com>
2020-09-02 02:17:37 +0430

    debug and any changes for input middleware

--- src/shared/components/FormElement/Input.js
+++ src/shared/components/FormElement/Input.js
@@ -22,10 +22,10 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.value||"",
+    value: props.initialValue||"",
     isTouch:false,
     // valid is in edit place and false is for add place form
-    isValid: props.valid ||false,
+    isValid: props.initialValue ||false,
   });
 
   const {id , onInput}= props
